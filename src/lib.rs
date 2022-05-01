011ee5e91094079a0a2923b87d9a35970980ae52
Markus Dam <markus.dam123@gmail.com>
2022-05-02 01:28:33 +0200

    Add randomness

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,50 @@
 use std::time::Duration;
+use rand::prelude::*;
 use rodio::{source::{Source, SamplesConverter}, buffer::SamplesBuffer};
 
 pub mod encoding;
 
-pub fn alias<I: Source>(samples: SamplesConverter<I, f32>, duration: Duration, factor: usize) -> SamplesBuffer<f32> where <I as Iterator>::Item: rodio::Sample {
-    let aliased = samples.take_duration(duration*factor as u32).step_by(factor);
-    SamplesBuffer::new(2, 44100, aliased.collect::<Vec<f32>>())
+#[derive(Clone)]
+pub struct AliasingParams {
+    pub factor: usize,
+    pub target_duration: Duration,
+    pub factor_variation: usize,
+}
+
+impl AliasingParams {
+    pub fn from_secs(target_duration_secs: f32, factor: usize, variation: usize) -> Self {
+        Self {
+            factor,
+            target_duration: Duration::from_secs_f32(target_duration_secs),
+            factor_variation: variation,
+        }
+    }
+}
+
+pub fn alias<I: Source>(samples: SamplesConverter<I, f32>, params: &AliasingParams) -> SamplesBuffer<f32> where <I as Iterator>::Item: rodio::Sample {
+    let duration = params.target_duration;
+    let factor = params.factor;
+    let variation = params.factor_variation as isize;
+    let aliased = if variation == 0 {
+        samples.take_duration(duration*factor as u32).step_by(factor).collect::<Vec<f32>>()
+    } else {
+        let sample_count = (duration.as_secs() * samples.sample_rate() as u64) as usize;
+        let samples: Vec<_> = samples.take_duration(duration*factor as u32).collect();
+        let mut v = Vec::with_capacity(sample_count);
+        let mut i = 0;
+        let mut rng = rand::thread_rng();
+        let mut collected = 0;
+
+        while collected < sample_count {
+            if let Some(s) = samples.get(i) {
+                v.push(*s);
+            } else {
+                break;
+            };
+            collected += 1;
+            i += (rng.gen_range(-variation..=variation) + factor as isize).max(0) as usize;
+        }
+        v
+    };
+    SamplesBuffer::new(2, 44100, aliased)
 } 
